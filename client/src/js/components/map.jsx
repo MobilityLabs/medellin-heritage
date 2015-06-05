b2ef2e9834a58cb31c77e227e74b138f146ed2ef
Mandy Moore <mandy@mobility-labs.com>
2015-06-05 13:59:39 -0500

    styled popup

--- client/src/js/components/map.jsx
+++ client/src/js/components/map.jsx
@@ -24,7 +24,7 @@ var Map = React.createClass({
   componentDidMount: function () {
     var items = MapStore.getHeritageItems();
     var map = this.map = L.map(this.getDOMNode(), {
-      center: [6.174469, -75.584556],
+      center: [6.237, -75.579],
       zoom: 15,
       minZoom: 2,
       maxZoom: 18
@@ -34,18 +34,37 @@ var Map = React.createClass({
       'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
-        id: 'mandym.0417cffe',
-        accessToken: 'pk.eyJ1IjoibWFuZHltIiwiYSI6InJ0N0t5UzQifQ.QuC-ffTnnah5oonnfh-hlQ'
+        id: 'rickymetz.9458305e',
+        accessToken: 'pk.eyJ1Ijoicmlja3ltZXR6IiwiYSI6IjQ0MDkxNWJiOTU0ZWJmMzNkMGVlODZiOWM0ODgwNmMwIn0.n3vqv0FdFTjASE09SDiUSg'
       }
     ).addTo(map);
 
-    var marker = new 
-    L.Icon({
-      iconUrl: "images/marker-icon.png"
+    var marker = new L.Icon({
+      iconUrl: 'images/marker-icon.png'
     });
 
-    function content (feature,layer) {
-      layer.bindPopup("<h1>" + feature.properties.Title + "</h1>");
+    function content (feature, layer) {
+      layer.bindPopup(
+        '<div class="container">' +
+          '<div class="row gallery">' +
+            '<div>' + feature.properties.ImageName + '</div>' + 
+          '</div>' +
+          '<div class="row info">' +
+            '<div class="details col-md-6 col-sm-12">' + 
+              '<p id="title">' + feature.properties.Title + '</p>' + 
+              '<p id="author">' + feature.properties.Author + '</p>' + 
+              '<p id="year">' + feature.properties.Year + '</p>' + 
+            '</div>' +
+            '<div class="sharing col-md-6 col-sm-12">' +
+              '<div class="row">' +
+                '<div class="col-md-4"><p>Share</p></div>' +
+                '<div class="col-md-4"><p>Learn More</p></div>' +
+                '<div class="col-md-4"><p>Directions</p></div>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+        );
       layer.setIcon(marker);
     };
 
--- client/src/js/seedData.js
+++ client/src/js/seedData.js
@@ -4,68 +4,176 @@ var seeds = {
     {
       "type": "Feature",
       "properties": {
-        "Type": "statue",
-        "Title": "Statue 2",
-        "Author": "Famous Artist",
-        "Date": 1957,
-        "Image Name": "artistname_piecename2.png"
+        "Title": "ESTRUCTURA CROMOVEGETAL",
+        "Author": "CARLOS CRUZ DIEZ",
+        "Date": 1983,
+        "Image Name (640 px. Max.)": "carloscruzdiez_estructuracromovegetal.png"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.583344,
-          6.175525
+          -75.579,
+          6.237
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "Type": "statue",
-        "Title": "Statue 3",
-        "Author": "Famous Artist",
-        "Date": 1958,
-        "Image Name": "artistname_piecename3.png"
+        "Title": "SIN TÍTULO",
+        "Author": "CARLOS ROJAS",
+        "Date": 1983,
+        "Image Name (640 px. Max.)": "carlosrojas_sintitulo.png"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.584728,
-          6.175035
+          -75.578,
+          6.235
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "Type": "statue",
-        "Title": "Statue 4",
-        "Author": "Famous Artist",
-        "Date": 1959,
-        "Image Name": "artistname_piecename4.png"
+        "Title": "SIN TÍTULO",
+        "Author": "JULIO LE PARC",
+        "Date": "",
+        "Image Name (640 px. Max.)": "julioleparc_sintitulo.png"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.582732,
-          6.176283
+          -75.581,
+          6.235
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "Type": "archsite",
-        "Title": "Statue 5",
-        "Author": "Famous Artist",
-        "Date": 1960,
-        "Image Name": "artistname_piecename5.png"
+        "Title": "SIN TÍTULO",
+        "Author": "ALBERTO URIBE DUQUE",
+        "Date": 1983,
+        "Image Name (640 px. Max.)": "albertouribe_sintitulo.png"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.582367,
-          6.175216
+          -75.58,
+          6.235
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "CACIQUE NUTIBARA",
+        "Author": "JOSE HORACIO BETANCUR",
+        "Date": 1955,
+        "Image Name (640 px. Max.)": "josehoraciobetancur_caciquenutibara.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.58,
+          6.236
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "GUILLERMO ZULUAGA \"MONTECRISTO\"",
+        "Author": "LUZ MARIA PIEDRAHITA",
+        "Date": 2000,
+        "Image Name (640 px. Max.)": "luzmariapierahita_guillermozuluaga.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.58,
+          6.235
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "\"M\" CROMO INTERFERENCIA",
+        "Author": "CARLOS CRUZ DIEZ",
+        "Date": 1985,
+        "Image Name (640 px. Max.)": "carloscruzdiez_mcromo.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.579,
+          6.256
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "SIMÓN BOLIVAR",
+        "Author": "JOSUE GIRALDO",
+        "Date": 1930,
+        "Image Name (640 px. Max.)": "josuegiraldo_simonbolivar.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.596,
+          6.232
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "GILBERTO ALZATE AVENDAÑO",
+        "Author": "GUSTAVO LÓPEZ",
+        "Date": 1978,
+        "Image Name (640 px. Max.)": "gustavolopez_gilbertoalzateavendano.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.602,
+          6.231
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "MARCO FIDEL SUAREZ",
+        "Author": "OCTAVIO MONTOYA",
+        "Date": 1954,
+        "Image Name (640 px. Max.)": "octaviomontoya_marcofidelsuarez.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.597,
+          6.244
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Title": "ATANASIO GIRARDOT",
+        "Author": "JOSE HORACIO BETANCUR",
+        "Date": 1954,
+        "Image Name (640 px. Max.)": "josehoraciobetancur_atanasiogirardot.png"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.591,
+          6.257
         ]
       }
     }
